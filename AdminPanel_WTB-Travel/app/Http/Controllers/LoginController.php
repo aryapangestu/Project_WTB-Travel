d2d51491c38c0f63fc7ddb736a60bfd2080dc434
aryapangestu <aryapangestu02@gmail.com>
2022-04-04 23:16:02 +0700

    fix minor

--- AdminPanel_WTB-Travel/app/Http/Controllers/LoginController.php
+++ AdminPanel_WTB-Travel/app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\app\Models\pengguna;
+use App\Models\pengguna;
 
 class LoginController extends Controller
 {
