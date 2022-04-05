800a78de25706f663f3a41fde30af52b34a4b01c
GNite12 <muhammadihsanadly7@gmail.com>
2022-04-05 15:07:08 +0700

    tambah tabel user

--- AdminPanel_WTB-Travel/resources/views/Dashboard/users.blade.php
+++ AdminPanel_WTB-Travel/resources/views/Dashboard/users.blade.php
@@ -17,93 +17,102 @@
 
                 <div class="card">
                     <div class="card-body">
+                    <h5 class="card-title">Datatables</h5>
+                    <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>
 
-                        <!-- Table with stripped rows -->
-                        <table class="table datatable" style="margin-top: 1.5rem;">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Muhammad Ihsan Adly</td>
-                                    <td>
-                                        <button type="submit" class="btn btn-dark rounded-pill"
-                                            style="width: fit-content; margin-right: 1rem;">
-                                            <a href="editusers.html" class="text-light">
-                                                Edit
-                                            </a>
-                                        </button>
-                                        <button type="submit" class="btn btn-dark rounded-pill" style="width: fit-content;">
-                                            <a href="deleteusers.html" class="text-light">
-                                                Delete
-                                            </a>
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Arya Pangestu</td>
-                                    <td>
-                                        <button type="submit" class="btn btn-dark rounded-pill"
-                                            style="width: fit-content; margin-right: 1rem;">
-                                            <a href="editusers.html" class="text-light">
-                                                Edit
-                                            </a>
-                                        </button>
-                                        <button type="submit" class="btn btn-dark rounded-pill" style="width: fit-content;">
-                                            <a href="deleteusers.html" class="text-light">
-                                                Delete
-                                            </a>
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Najla Nur Adila</td>
-                                    <td>
-                                        <button type="submit" class="btn btn-dark rounded-pill"
-                                            style="width: fit-content; margin-right: 1rem;">
-                                            <a href="editusers.html" class="text-light">
-                                                Edit
-                                            </a>
-                                        </button>
-                                        <button type="submit" class="btn btn-dark rounded-pill" style="width: fit-content;">
-                                            <a href="deleteusers.html" class="text-light">
-                                                Delete
-                                            </a>
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th>
-                                    <td>Putri Samapa Hutapea</td>
-                                    <td>
-                                        <button type="submit" class="btn btn-dark rounded-pill"
-                                            style="width: fit-content; margin-right: 1rem;">
-                                            <a href="editusers.html" class="text-light">
-                                                Edit
-                                            </a>
-                                        </button>
-                                        <button type="submit" class="btn btn-dark rounded-pill" style="width: fit-content;">
-                                            <a href="deleteusers.html" class="text-light">
-                                                Delete
-                                            </a>
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!-- End Table with stripped rows -->
+                    <!-- Table with stripped rows -->
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Fullname</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">IsAdmin</th>
+                            <th scope="col">Created at</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Muhammad Ihsan Adly</td>
+                            <td>GNite12</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                            </div>
+                            </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                                </div>
+                            </td>
+                            <td>
+                                15:01 05/04/2022
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Arya Pangestu</td>
+                            <td>Brevizz</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                            </div>
+                            </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                                </div>
+                            </td>
+                            <td>
+                                n.d.
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Najla Nur Adila</td>
+                            <td>Najla</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                            </div>
+                            </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                                </div>
+                            </td>
+                            <td>
+                                n.d.
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>Putri Samapa Hutapea</td>
+                            <td>Putri</td>
+                            <td>
+                                <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                            </div>
+                            </td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                                </div>
+                            </td>
+                            <td>
+                                n.d.
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <!-- End Table with stripped rows -->
 
                     </div>
                 </div>
 
-            </div>
+                </div
         </div>
     </section>
 @endsection
