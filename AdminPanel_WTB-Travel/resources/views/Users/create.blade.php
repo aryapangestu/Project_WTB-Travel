35d05f349cb3cd732308a9bee5f103064fe1d065
najlanuradila <najlaadila1808@gmail.com>
2022-04-11 23:52:02 +0700

    nambahcreate

---
+++ AdminPanel_WTB-Travel/resources/views/Users/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('konten')
+    <div class="pagetitle">
+        <h1>Create Account</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item">Users</li>
+                <li class="breadcrumb-item active">Create</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+@endsection
