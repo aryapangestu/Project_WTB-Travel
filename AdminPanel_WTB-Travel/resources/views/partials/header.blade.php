d0e6faacb383921c4c30e2325d842105a80134e9
aryapangestu <aryapangestu02@gmail.com>
2022-04-13 16:13:25 +0700

    header

--- AdminPanel_WTB-Travel/resources/views/partials/header.blade.php
+++ AdminPanel_WTB-Travel/resources/views/partials/header.blade.php
@@ -2,9 +2,8 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="/" class="logo d-flex align-items-center">
-            <img src="{{ URL::asset('assets/img/logo.png') }}" alt="">
-            <span class="d-none d-lg-block">WTB Travel</span>
+        <a href="/" class="d-flex align-items-center">
+            <img src="{{ URL::asset('assets/img/logo.png') }}">
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
