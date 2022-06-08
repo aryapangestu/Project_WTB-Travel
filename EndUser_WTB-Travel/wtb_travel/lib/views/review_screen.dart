23fcd324de60ed906183ec989db697393e40cd97
aryapangestu <aryapangestu02@gmail.com>
2022-06-08 21:40:06 +0700

    update review

--- EndUser_WTB-Travel/wtb_travel/lib/views/review_screen.dart
+++ EndUser_WTB-Travel/wtb_travel/lib/views/review_screen.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:wtb_travel/views/detail_place_screen.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class Review {
+  String title;
+  String? subtitle;
+  String? rating;
+
+  Review({required this.title, this.subtitle, this.rating});
+}
 
 class WtbTravelReviewScreen extends StatefulWidget {
   const WtbTravelReviewScreen({Key? key}) : super(key: key);
@@ -8,160 +18,118 @@ class WtbTravelReviewScreen extends StatefulWidget {
   State<WtbTravelReviewScreen> createState() => _WtbTravelReviewScreen();
 }
 
-class _ArticleDescription extends StatelessWidget {
-  const _ArticleDescription({
-    Key? key,
-    required this.title,
-    required this.subtitle,
-    required this.author,
-  }) : super(key: key);
-
-  final String title;
-  final String subtitle;
-  final String author;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-          flex: 12,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                title,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
-              Text(
-                subtitle,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 15.5,
-                  color: Colors.black87,
-                ),
-              ),
-              RatingBar.builder(
-                itemSize: 18,
-                initialRating: 4,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                onRatingUpdate: (rating) {
-                  print(rating);
-                },
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                author,
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.black87,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class CustomListItemTwo extends StatelessWidget {
-  const CustomListItemTwo({
-    Key? key,
-    required this.thumbnail,
-    required this.title,
-    required this.subtitle,
-    required this.author,
-  }) : super(key: key);
-
-  final Widget thumbnail;
-  final String title;
-  final String subtitle;
-  final String author;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
-      child: SizedBox(
-        height: 100,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 1.0,
-              child: thumbnail,
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
-                child: _ArticleDescription(
-                  title: title,
-                  subtitle: subtitle,
-                  author: author,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class _WtbTravelReviewScreen extends State<WtbTravelReviewScreen> {
+  List<Review> review = [
+    Review(
+      title: "Ihsan",
+      subtitle: 'Bagus museumnya',
+      rating: '4.0',
+    ),
+    Review(
+      title: "Ihsan",
+      subtitle: 'Bagus museumnya',
+      rating: '4.0',
+    ),
+    Review(
+      title: "Ihsan",
+      subtitle: 'Bagus museumnya',
+      rating: '4.0',
+    ),
+    Review(
+      title: "Ihsan",
+      subtitle: 'Bagus museumnya',
+      rating: '4.0',
+    ),
+    Review(
+      title: "Ihsan",
+      subtitle: 'Bagus museumnya',
+      rating: '4.0',
+    ),
+  ];
+
+  @override
+  void initState() {
+    setStatusBarColor(const Color(0xfff6f0e4));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    setStatusBarColor(const Color(0xff88692b));
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(10.0),
-      children: <Widget>[
-        CustomListItemTwo(
-          thumbnail: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'https://cdn-icons-png.flaticon.com/512/33/33308.png'))),
-          ),
-          title: 'Gedung Sate',
-          subtitle: 'Enak satenya',
-          author: 'James Bond',
-        ),
-        CustomListItemTwo(
-          thumbnail: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'https://cdn-icons-png.flaticon.com/512/33/33308.png'))),
-          ),
-          title: 'Museum Geologi',
-          subtitle: 'Bagus museumnya',
-          author: 'Ihsan',
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Wrap(
+        spacing: 16,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        direction: Axis.vertical,
+        runAlignment: WrapAlignment.start,
+        crossAxisAlignment: WrapCrossAlignment.start,
+        runSpacing: 8,
+        children: review.map((e) {
+          return Container(
+            width: context.width() - 32,
+            decoration: BoxDecoration(
+                color: context.cardColor, borderRadius: radius(32)),
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    16.height,
+                    Text(e.title,
+                            style: boldTextStyle(
+                                size: 18, color: const Color(0xff543c0d)))
+                        .paddingSymmetric(horizontal: 8),
+                    4.height,
+                    Text(e.subtitle!,
+                            style: secondaryTextStyle(
+                                color: const Color(0xffc7b899), size: 12))
+                        .paddingSymmetric(horizontal: 8),
+                    4.height,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            RatingBar.builder(
+                              initialRating: e.rating!.toDouble(),
+                              minRating: 5,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemSize: 18,
+                              itemPadding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              itemBuilder: (context, _) => const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              onRatingUpdate: (rating) {
+                                //
+                              },
+                            ),
+                            4.width,
+                            Text(e.rating!, style: boldTextStyle()),
+                            2.width,
+                          ],
+                        ),
+                      ],
+                    ).paddingSymmetric(horizontal: 8),
+                    16.height,
+                  ],
+                ),
+              ],
+            ),
+          ).onTap(() {
+            // const WtbTravelDetailPlaceScreen(element: e).launch(context);
+            const WtbTravelDetailPlaceScreen().launch(context);
+          });
+        }).toList(),
+      ).paddingOnly(top: 16, bottom: 70, left: 16, right: 16),
     );
   }
 }
