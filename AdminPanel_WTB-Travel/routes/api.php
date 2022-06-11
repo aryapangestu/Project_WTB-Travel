1310655cdbf86cdc6d8e011f5643f34f84bef617
aryapangestu <aryapangestu02@gmail.com>
2022-06-11 20:40:50 +0700

    API controller

--- AdminPanel_WTB-Travel/routes/api.php
+++ AdminPanel_WTB-Travel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PlaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->group(function () {
+//     Route::apiResource('places', PlaceController::class);
+// });
+
+Route::apiResource('places', PlaceController::class);
