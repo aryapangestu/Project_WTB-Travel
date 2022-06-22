20c900da5d2255aadd5217a27c181cc513280f72
aryapangestu <aryapangestu02@gmail.com>
2022-06-22 21:37:54 +0700

    fix category

--- AdminPanel_WTB-Travel/app/Http/Controllers/API/CategoryController.php
+++ AdminPanel_WTB-Travel/app/Http/Controllers/API/CategoryController.php
@@ -19,9 +19,48 @@ class CategoryController extends Controller
         return response()->json($categories);
     }
 
-    public function getReview($id)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        $categories = Category::where('place_id', $id)->get();
-        return response()->json($categories);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
